29b2f63e7b2b305b9d1d469992d48e875a86d862
Philip Woelfel <philip@woelfel.at>
2016-10-20 00:44:22 +0200

    added diagramtype to diagramcell

--- src/at/woelfel/philip/quiver/objects/DiagramCell.groovy
+++ src/at/woelfel/philip/quiver/objects/DiagramCell.groovy
@@ -6,8 +6,25 @@ import at.woelfel.philip.quiver.objects.Cell.Type
  * Created by philip on 19.06.16.
  */
 class DiagramCell extends Cell {
-
+	
+	public enum DiagramType {
+		SEQUENCE,
+		FLOWCHART
+	}
+	
+	DiagramType diagramType
+	
 	public DiagramCell(){
 		type = Type.DIAGRAM
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder()
+
+		sb.append("Diagram Cell\n")
+		sb.append("\tDiagram Type: $diagramType\n")
+		sb.append("\tData: $data\n")
+
+		return sb.toString()
 	}
 }
